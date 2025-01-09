a02b29a557a1d76a8f759b1a2f16a1ab907615cb
Gargi Lohia <gargilohia2001@yahoo.com>
2025-01-09 17:57:20 +0900

    integrate Groq client for journal response fetching and update API key handling(WIP)

--- api/fetch_journal_response.py
+++ api/fetch_journal_response.py
@@ -3,10 +3,10 @@ from flask import Flask, request
 import requests
 from dotenv import load_dotenv
 import os
+from groq import Groq
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
 
 async def fetch_journal_response(data):
     """Fetches the journal response for a given journal entry from an api endpoint.
@@ -17,7 +17,29 @@ async def fetch_journal_response(data):
     Returns:
         A dictionary containing the journal response.
     """
-    print("aaaaa", API_KEY)
+    client = Groq(api_key=os.environ.get("GROQ_API_KEY"), )
+    print("aaaaa", os.environ.get("GROQ_API_KEY"))
+    system_prompt = {
+    "role": "system",
+    "content":
+    "You are a helpful assistant. You reply with very short answers."
+    }
+    chat_history = [system_prompt]
+    user_input = data
+    chat_history.append({"role": "user", "content": user_input})
+    response = client.chat.completions.create(model="llama3-70b-8192",
+                                            messages=chat_history,
+                                            max_tokens=100,
+                                            temperature=1.2)
+    chat_history.append({
+      "role": "assistant",
+      "content": response.choices[0].message.content
+    })
+    # Print the response
+    print("Assistant:", response.choices[0].message.content)
+
+
+    
     data = {
         "model": "llama-3.3-70b-versatile",
         "messages": [{
